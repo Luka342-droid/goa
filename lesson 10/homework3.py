515a6c2e6833f2c16857ae71b6dd44bf32dda7ef
Luka342-droid <lamashukeli846@gmail.com>
2024-11-13 22:35:18 +0400

    Delete lesson 10 directory

--- lesson 10/homework3.py
+++
@@ -1,8 +0,0 @@
-# ასაკის შეყვანა
-age = int(input("enter your age: "))
-
-# calculating age in 10 years
-age_in_10_years = age + 10
-
-# შედეგის დაბეჭდვა
-print(age_in_10_years)
--- lesson 10/homework6.py
+++
@@ -1,8 +0,0 @@
-num = int(input("enter your number: "))
-num2 = int(input("enter your number: "))
-num3 = int(input("enter your number: "))
-
-sum = num + num2 + num3
-diff = num - num2 - num3
-multiplied = num * num2 * num3
-division = num / num2 / num3
